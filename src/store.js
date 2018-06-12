1bde21e9a570b566674a39ff32e46e43ac907d57
Mehul Thakkar <mehulthakkar02@gmail.com>
2018-06-12 09:13:53 +0530

    Replaced hash history with browser history

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { routerMiddleware } from 'react-router-redux';
-import createHistory from 'history/createHashHistory';
+import createHistory from 'history/createBrowserHistory';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import apiMiddleware from './middleware';
